742bd87edf27c82da9f0a57a42a553a9dc74e5cf
henning410 <henning1.weise@web.de>
2024-06-11 17:17:58 +0200

    Update main.ts

--- noauth/src/main.ts
+++ noauth/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   const port: number = config.get<number>("PORT");
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle("VuNAPI - no Auth")
+    .setTitle("VuNAPI - No Auth")
     .setDescription("OpenAPI v3 specs for VuNAPI")
     .setVersion("1.0")
     .addServer(base_url, "Local environment")
